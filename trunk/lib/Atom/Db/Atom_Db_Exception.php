ae21dfba36c89c349090f4c1ffa567516aa056ed
marc.steinert@rwth-aachen.de <marc.steinert@rwth-aachen.de@5b4c4a33-b75b-ff7d-8c2c-646da25a7ad0>
2010-09-06 09:49:02 +0000

    - Added rudimentary Atom_Db namespace for database access

---
+++ trunk/lib/Atom/Db/Atom_Db_Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Atom framework
+ * 
+ * @version $Id$
+ * @copyright Marc Steinert
+ */
+
+/**
+ * Base exception for the Atom_Db pseudo namespace.
+ * 
+ * @uses    Atom_Exception
+ */
+class Atom_Db_Exception extends Atom_Exception { }
