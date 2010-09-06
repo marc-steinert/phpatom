5f748fca5d9298d5017a27596230fe28deef2de1
marc.steinert@rwth-aachen.de <marc.steinert@rwth-aachen.de@5b4c4a33-b75b-ff7d-8c2c-646da25a7ad0>
2010-09-06 10:36:24 +0000

    - Added Atom_Db_Result for encapsulating database results.

---
+++ trunk/lib/Atom/Db/Atom_Db_Result.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Atom framework
+ * 
+ * @version $Id$
+ * @copyright Marc Steinert
+ */
+
+/**
+ * Represents a query result.
+ *
+ * @uses        Iterator
+ * @uses        Countable
+ * @uses        ArrayAccess
+ * @package     Atom Framework
+ * @author      Marc Steinert <marc@bithub.net>
+ */
+final class Atom_Db_Result implements Iterator, Countable, ArrayAccess {
+
+    /** @var recource */
+    private $_result;
+
+    /** @var int */
+    private $_numRows;
+
+    /** @var int */
+    private $_index = 0;
+
+    /** @var array */
+    private $_data;
+
+
+    /**
+     *
+     * @throws  Atom_Db_Exception
+     *
+     * @param resource $result
+     */
+    public function __construct($result) {
+        if (!is_resource($result)) {
+            throw new Atom_Db_Exception('No valid SQL result given.');
+        }
+
+        $this->_result = $result;
+        $this->_numRows = mysql_num_rows($this->_result);
+
+        while(($row = mysql_fetch_assoc($this->_result))) {
+            $this->_data[] = $row;
+        }
+    }
+    
+    public function __get($key) {
+        return $this->_data[$this->_index][$key];
+    }
+
+    public function __isset($key) {
+        return array_key_exists($this->_data[$this->_index], $key);
+    }
+
+    /**
+     * Fetches all rows targeted by the db result
+     *
+     * @return array
+     */
+    public function fetchAll() {
+        return $this->_data;
+    }
+
+    /**#@+
+     * Implemention of Iterator-interface
+     */
+    public function rewind() {
+        $this->_index = 0;
+    }
+
+    public function valid() {
+        return ($this->_index + 1 <= $this->_numRows);
+    }
+
+    public function current() {
+        return $this->_data[$this->_index];
+    }
+
+    public function key() {
+        return $this->_index;
+    }
+
+    public function next() {
+        $this->_index++;
+    }
+    /**#@-*/
+
+    /**#@+
+     * Implemention of Countable-Interface
+     */
+    /**
+     * Gets number of rows of the current SQL result
+     *
+     * @return int
+     */
+    public function count() {
+        return $this->_numRows;
+    }
+    /**#@-*/
+
+    /**#@+
+     * Implemention of ArrayAccess-Interface
+     */
+    public function offsetExists($offset) {
+        return isset($this->_data[$this->_index][$offset]);
+    }
+
+    public function offsetSet($offset, $value) {
+        $this->_data[$this->_index][$offset] = $value;
+    }
+
+    public function offsetGet($offset) {
+        return $this->_data[$this->_index][$offset];
+    }
+
+    public function offsetUnset($offset) { }
+    /**#@-*/
+}
