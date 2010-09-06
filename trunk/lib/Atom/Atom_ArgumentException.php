76d81deccdb0cb581ec679f041ec59aa69b1c90d
marc.steinert@rwth-aachen.de <marc.steinert@rwth-aachen.de@5b4c4a33-b75b-ff7d-8c2c-646da25a7ad0>
2010-09-06 07:47:57 +0000

    - Added all framework base exceptions

---
+++ trunk/lib/Atom/Atom_ArgumentException.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Atom framework
+ * 
+ * @version $Id$
+ * @copyright Marc Steinert
+ */
+ 
+/**
+ * Generic exception, gets thrown for invalid or malformed
+ *  arguments passed to function or methods.
+ * 
+ * @uses        Atom_Exception
+ * @package     Atom Framework
+ * @author      Marc Steinert <marc@bithub.net>
+ */
+class Atom_ArgumentException extends Atom_Exception { }
---
+++ trunk/lib/Atom/Atom_SystemException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Atom framework
+ *
+ * @version $Id$
+ * @copyright Marc Steinert
+ */
+
+/**
+ * Gets thrown if framework parts are used other than intended.
+ *
+ * @uses        Atom_Exception
+ * @package     Atom Framework
+ * @author      Marc Steinert <marc@bithub.net>
+ */
+class Atom_SystemException extends Atom_Exception { }
