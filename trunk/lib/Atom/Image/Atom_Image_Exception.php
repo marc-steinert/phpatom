a1a11e6742813f7766a341098d34cab1bea2364c
marc.steinert@rwth-aachen.de <marc.steinert@rwth-aachen.de@5b4c4a33-b75b-ff7d-8c2c-646da25a7ad0>
2010-09-06 08:57:40 +0000

    - Added Atom_Image pseudo namespace, which provides a wrapper for GD functions

---
+++ trunk/lib/Atom/Image/Atom_Image_Exception.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Atom framework
+ * 
+ * @version $Id$
+ * @copyright Marc Steinert
+ */
+ 
+/**
+ * Base exception for image operations.
+ * 
+ * @uses        Atom_Exception
+ * @package     Atom Framework
+ * @author      Marc Steinert <marc@bithub.net>
+ */
+class Atom_Image_Exception extends Atom_Exception { }
