3ebbd20da901a8b3de4a7737d18ca55a29f7d363
marc.steinert@rwth-aachen.de <marc.steinert@rwth-aachen.de@5b4c4a33-b75b-ff7d-8c2c-646da25a7ad0>
2010-09-06 08:35:10 +0000

    - Added generic config storage class - Added base exception for missing files

---
+++ trunk/lib/Atom/Atom_FileNotFoundException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Atom framework
+ *
+ * @version $Id$
+ * @copyright Marc Steinert
+ */
+
+/**
+ * Gets thrown, if an input file is not found by the framework.
+ *
+ * @ueses       Atom_Exception
+ * @package     Atom Framework
+ * @author      Marc Steinert <marc@bithub.net>
+ */
+class Atom_FileNotFoundException extends Atom_Exception { }
